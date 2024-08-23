d86c2bc8fe364380d7acf9dd0ff8027615e37151
ctcakip1 <tinigaming058@gmail.com>
2024-08-23 21:55:49 +0700

    #134

--- src/components/header/app.header.tsx
+++ src/components/header/app.header.tsx
@@ -63,7 +63,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 
 export default function AppHeader() {
     const { data: session } = useSession();
-    console.log("check session: ", session);
+    console.log("check sesssion: ", session);
     const router = useRouter();
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
     const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
